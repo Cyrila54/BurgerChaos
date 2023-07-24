879e45ac73c140460955694d72b2f1d8a075f952
Cyril <120710526+Cyrila54@users.noreply.github.com>
2023-07-24 20:31:05 +0200

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <div className={styles.footer_container}>
-      <p>Le site <span>Crazy Burger</span> est un modèle original créé et inventé par Cyril ANDRIQUE.<br/> Toute reproduction est interdite.
+      <p>Le site <span>Crazy Burger</span> est un modèle original de site e-commerce créé et inventé par Cyril ANDRIQUE.<br/> Toute reproduction est interdite.
       </p>
     </div>
   );
