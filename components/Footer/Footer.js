59b7418ad5724b1e00ed5802bd0de33df978d3a8
Cyril <120710526+Cyrila54@users.noreply.github.com>
2023-07-27 20:41:09 +0200

    up

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from "react";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <div className={styles.footer_container}>
-      <p>Le site <span>Crazy Burger</span> est un modèle original de site e-commerce créé et inventé par Cyril ANDRIQUE.<br/> Toute reproduction est interdite.
-      </p>
+      <div className={styles.logo}>
+        <img src="burger.png" alt="burger" className={styles.imglogo} />
+        <div className={styles.textLogo}>
+          <p>Crazy</p>
+          <p>Burger</p>
+        </div>
+      </div>
+      <div className={styles.textFooter}>
+        <p>contactez-nous</p>
+      </div>
+
+      <div className={styles.cards}>
+        Paiement accepté
+        <img src="100677181517645217876.png" alt="" />
+      </div>
     </div>
   );
 }
+
+/*         <p>
+          Le site <span>Crazy Burger</span> est un modèle original de site
+          e-commerce créé et inventé par Cyril ANDRIQUE.
+          <br /> Toute reproduction est interdite.
+        </p>; */
